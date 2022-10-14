51da0c1e0922bd79e98ded0206055db56c5152c4
Shaun <772542545@qq.com>
2022-10-14 10:31:34 +0800

    .

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from celery import Celery
-import tasks
-import trio
+from tasks import Task
 
 
 app = Celery("tasks", broker='redis://192.168.3.42:6379/0', backend='redis://192.168.3.42:6379/0')
@@ -10,4 +9,5 @@ app = Celery("tasks", broker='redis://192.168.3.42:6379/0', backend='redis://192
 
 @app.task
 def http():
-    trio.run(tasks.Task)
+    Task()
+    
--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,27 +1,38 @@
-from asyncinit import asyncinit
 import trio
-from . import trio_test
+import httpx
+from datetime import datetime
 
 
 
-@asyncinit
 class Task:
-    async def __init__(self):
-        await self.http()
-    
-
-    async def add(self,x, y):
-        return x + y
-
+    def __init__(self):
+        trio.run(self.http)
+        self.test()
 
 
     async def http(self):
         async with trio.open_nursery() as nursery:
-            nursery.start_soon(trio_test.test)
-            nursery.start_soon(trio_test.test)
-            nursery.start_soon(trio_test.test)
-            nursery.start_soon(trio_test.test)
-            nursery.start_soon(trio_test.test)
-            nursery.start_soon(trio_test.test)
-            nursery.start_soon(trio_test.test)
-            nursery.start_soon(trio_test.test)
+            nursery.start_soon(self.clent)
+            nursery.start_soon(self.clent)
+            nursery.start_soon(self.clent)
+            nursery.start_soon(self.clent)
+            nursery.start_soon(self.clent)
+            nursery.start_soon(self.clent)
+            nursery.start_soon(self.clent)
+            nursery.start_soon(self.clent)
+
+
+    async def clent(self):
+        try:
+            async with httpx.AsyncClient() as client:  # 创建一个异步client
+                # 1/0
+                r = await client.get('https://www.example.com/')
+                print(r)
+                print(datetime.now())
+        except Exception as e:
+            print(str(e))
+
+    def test(self):
+        print("这里是测试。。。")
+
+
